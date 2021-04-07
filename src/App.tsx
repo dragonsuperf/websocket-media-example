2d9fd7f9fa655c13cdb7ffb87e2caef83f4cdbc9
dragonsuperf <dragonsuperf@gmail.com>
2021-04-07 23:16:35 +0900

    api로 데이터 받아와서 검색 목록 띄워 오는 것까지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,27 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+interface YoutubeChannelData {
+  etag: string;
+  id: {
+    kind: string;
+    videoId: string;
+  }
+  snippet: {
+    publishedAt: Date;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      }
+    }
+  }
+}
+
 function VideoSection() {
   return (
     <video autoPlay src="./hunchgray.mp4#t=15" />
@@ -9,12 +30,15 @@ function VideoSection() {
 }
 
 function App() {
+  const [result, setResult] = useState<YoutubeChannelData[]>([]);
   const [enter, setEnter] = useState(false);
+  const [query, setQuery] = useState('');
 
   const search = async () => {
     try {
       const response = await axios.get('https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&key=AIzaSyBCkatHNqut1afZOtYRgCs-lWUNheWJRls&q=hunchgray');
-      console.log(response);
+      console.log(response.data.items);
+      setResult(response.data.items);
     } catch(error) {
       console.error(error);
     }
@@ -23,6 +47,18 @@ function App() {
   return (
     <div className="App">
       {enter && <VideoSection />}
+      {result.length > 0 && result.map((item: YoutubeChannelData) => {
+        return (
+          <>
+            <img src={item.snippet.thumbnails.default.url} />
+            <p>{item.snippet.title}</p>
+          </>
+        );
+      })}
+      <input value={query} onChange={e => setQuery(e.target.value)} />
+      <button onClick={() => {
+        search();
+      }}>Search</button>
       <button onClick={() => {
         search();
         setEnter(!enter);
