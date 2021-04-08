900024eb5c8434d8599d2ccc13839f0887fc3eb3
dragonsuperf <dragonsuperf@gmail.com>
2021-04-08 11:36:29 +0900

    lint, prettier 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface YoutubeChannelData {
   id: {
     kind: string;
     videoId: string;
-  }
+  };
   snippet: {
     publishedAt: Date;
     channelId: string;
@@ -18,15 +18,13 @@ interface YoutubeChannelData {
         url: string;
         width: number;
         height: number;
-      }
-    }
-  }
+      };
+    };
+  };
 }
 
 function VideoSection() {
-  return (
-    <video autoPlay src="./hunchgray.mp4#t=15" />
-  );
+  return <video autoPlay src="./hunchgray.mp4#t=15" />;
 }
 
 function App() {
@@ -36,33 +34,44 @@ function App() {
 
   const search = async () => {
     try {
-      const response = await axios.get('https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&key=AIzaSyBCkatHNqut1afZOtYRgCs-lWUNheWJRls&q=hunchgray');
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&key=AIzaSyBCkatHNqut1afZOtYRgCs-lWUNheWJRls&q=${query}`,
+      );
       console.log(response.data.items);
       setResult(response.data.items);
-    } catch(error) {
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <div className="App">
       {enter && <VideoSection />}
-      {result.length > 0 && result.map((item: YoutubeChannelData) => {
-        return (
-          <>
-            <img src={item.snippet.thumbnails.default.url} />
-            <p>{item.snippet.title}</p>
-          </>
-        );
-      })}
-      <input value={query} onChange={e => setQuery(e.target.value)} />
-      <button onClick={() => {
-        search();
-      }}>Search</button>
-      <button onClick={() => {
-        search();
-        setEnter(!enter);
-      }}>Video</button>
+      {result.length > 0 &&
+        result.map((item: YoutubeChannelData) => {
+          return (
+            <>
+              <img src={item.snippet.thumbnails.default.url} alt="thumbnail" />
+              <p>{item.snippet.title}</p>
+            </>
+          );
+        })}
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <button
+        onClick={() => {
+          search();
+        }}
+      >
+        Search
+      </button>
+      <button
+        onClick={() => {
+          search();
+          setEnter(!enter);
+        }}
+      >
+        Video
+      </button>
     </div>
   );
 }
